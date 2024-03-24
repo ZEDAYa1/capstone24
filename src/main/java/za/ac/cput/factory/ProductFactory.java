7322f746045c17429aec83d30112410a3ee977f3
ZEDAYa1 <220097429@mycput.ac.za>
2024-03-24 18:11:16 +0200

    ProductFactory.java and ProductImageFactory.java factories complete.

--- src/main/java/za/ac/cput/factory/ProductFactory.java
+++ src/main/java/za/ac/cput/factory/ProductFactory.java
@@ -1,4 +1,4 @@
-package za.ac.factory;
+package za.ac.cput.factory;
 /*
  *Product:java
  *Product: Factory Class
@@ -6,6 +6,25 @@ package za.ac.factory;
  * Date: 21 March 2024
  */
 
-public class ProductFactory {
+import za.ac.cput.domain.Product;
+import za.ac.cput.util.Helper;
 
+public class ProductFactory {
+    public static Product createProduct(int productID, int categoryID, String name, String description, double price, int stock, int reviewID, int imageID){
+        if(Helper.isNullOrEmpty(name)
+        ||Helper.isNullOrEmpty(description)){
+            return null;
+        }
+
+        Product product = new Product.Builder().setProductID(productID)
+                .setCategoryID(categoryID)
+                .setName(name)
+                .setDescription(description)
+                .setPrice(price)
+                .setStock(stock)
+                .setReviewID(reviewID)
+                .setImageID(imageID)
+                .build();
+        return product;
+    }
 }
