438eb67ea746b93acf2967e700be8431482fe63a
Twist1154n <rethabile1154n@gmail.com>
2024-03-26 18:04:10 +0200

    Rethabile Ntsekhe(220455430) 26/March/2024 OrderFactory.java completed OrderFactory methods completed

--- src/main/java/za/ac/cput/factory/OrderFactory.java
+++ src/main/java/za/ac/cput/factory/OrderFactory.java
@@ -2,10 +2,14 @@ package za.ac.cput.factory;
 
 import za.ac.cput.domain.*;
 import za.ac.cput.util.Helper;
-
 import java.time.LocalDateTime;
 import java.util.List;
-
+/*
+ *OrderFactory: java
+ *Order:factory Class
+ * Author: Rethabile Ntsekhe (220455430)
+ * Date: 24 March 2024
+ */
 public class OrderFactory {
     public static Order createOrder(int orderID, LocalDateTime orderDate, Customer customer,
                                     List<OrderItem> orderItems, Double totalPrice, String status,
